0107ce401527ed0c3cfdada60834f32aa88b4838
Lucian Gutu <lucian@climb.ro>
2024-01-11 17:50:26 +0200

    Update progress_v4.py

--- progress_v4.py
+++ progress_v4.py
@@ -8,9 +8,22 @@ def print_progress_bar(iteration, total, prefix='', suffix='', length=30, fill='
     sys.stdout.write(f'\r{prefix} |{bar}| {percent}% {suffix}')
     sys.stdout.flush()
 
+def print_descending_progress_bar(iteration, total, prefix='', suffix='', length=30, fill='█'):
+    percent = "{0:.1f}".format(100 * ((total - iteration) / float(total)))
+    filled_length = int(length * (total - iteration) // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    sys.stdout.write(f'\r{prefix} |{bar}| {percent}% {suffix}')
+    sys.stdout.flush()
+
 total_iterations = 100
 for i in range(total_iterations + 1):
     time.sleep(0.1)
     print_progress_bar(i, total_iterations, prefix='Progress:', suffix='Complete', length=50)
 print()
 
+
+total_iterations = 100
+for i in range(total_iterations + 1):
+    time.sleep(0.1)
+    print_descending_progress_bar(i, total_iterations, prefix='Progress:', suffix='Complete', length=50)
+print()
